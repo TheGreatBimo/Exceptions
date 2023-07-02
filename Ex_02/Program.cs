eab9771c5f7fe2db7f48e8c8b5d2b365f1ce8725
User <User@WIN-UP6PGVKR45H>
2023-07-03 00:17:34 +0500

    Replace AddPrice method to AddProduct method

--- Ex_02/Program.cs
+++ Ex_02/Program.cs
@@ -25,24 +25,20 @@ namespace Ex_02
 			Product product_HPPrinter = new Product("HP Laserjet Printer");
 
 			market["A-01"].AddProduct(product_LGLcd, 650000);
-			market["A-01"].AddProduct(product_LGLcd, 650000);
-			market["A-01"].AddProduct(product_LGLcd, 650000);
-			market["A-01"].AddProduct(product_LGLcd, 650000);
-			
-			product_LGLcd.AddPrice(new Price(650000, market["A-01"]));
-			product_LGLcd.AddPrice(new Price(639000, market["A-02"]));
-			product_LGLcd.AddPrice(new Price(645000, market["B-01"]));
-			product_LGLcd.AddPrice(new Price(650000, market["B-02"]));
+			market["A-02"].AddProduct(product_LGLcd, 639000);
+			market["B-01"].AddProduct(product_LGLcd, 645000);
+			market["B-02"].AddProduct(product_LGLcd, 650000);
 
-			product_Iphone13pro.AddPrice(new Price(10000000, market["A-01"]));
-			product_Iphone13pro.AddPrice(new Price(9700000, market["B-01"]));
+			market["A-01"].AddProduct(product_Iphone13pro, 10000000);
+			market["B-01"].AddProduct(product_Iphone13pro, 9700000);
 
-			product_SeagateHDD1Tb.AddPrice(new Price(560000, market["A-02"]));
-			product_SeagateHDD1Tb.AddPrice(new Price(520000, market["B-02"]));
+			market["A-02"].AddProduct(product_SeagateHDD1Tb, 560000);
+			market["B-02"].AddProduct(product_SeagateHDD1Tb, 520000);
 
-			product_HPPrinter.AddPrice(new Price(1160000, market["A-01"]));
-			product_HPPrinter.AddPrice(new Price(1150000, market["B-01"]));
-			product_HPPrinter.AddPrice(new Price(1155000, market["B-02"]));
+			market["A-01"].AddProduct(product_HPPrinter, 1160000);
+			market["A-02"].AddProduct(product_HPPrinter, 1150000);
+			market["A-02"].UpdateProduct(product_HPPrinter, 1155000);
+			market["B-01"].AddProduct(product_HPPrinter, 1158000);
 
 			Console.WriteLine("Enter the name of shop to list products from:");
 			string shopname = Console.ReadLine();
